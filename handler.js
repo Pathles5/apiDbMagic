b2863db9e5f2289e4178c246e8deacf0b93b5b3e
Pathles5 <antonio.lopez.sarmiento@gmail.com>
2022-11-06 13:57:44 +0100

    *

--- handler.js
+++ handler.js
@@ -8,7 +8,12 @@ const { getCardById, getCardByAttribute, getCardsLegal } = require('./controller
 const app = express();
 
 app.use(express.json());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.get( // id
   '/cards/:cardId',
   check.param('cardId').exists(),
